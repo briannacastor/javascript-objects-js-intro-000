d46db1ed7950ce8580d3970f6fe79181ea621782
capable-optimization-2034 <brianna.l.castor@gmail.com>
2020-10-02 01:03:11 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist= {
   theBreeders: "cannonball",
   alGreen: "callMe",
   samCooke: "cupid"
-}
+};
 function updatePlaylist(playlist, madonna, likeAPrayer){
   return Object.assign({},
     playlist,{[madonna]:likeAPrayer});
